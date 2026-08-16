5d56606d1cce4b3cd3c450acc345a2ef6017074f
Jatin Yadav <jatinrao@7458>
2026-08-16 21:28:54 +0530

    feat: adding the intial routes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ const PORT =300
 app.get('/',(req,res)=>{
     res.send("Welcome to the Blogify API")
 })
+app.get('/api/v1/posts',(req,res)=>{
+    res.send('Sending all blogs...')
+})
+const post = [
+    {id:1,tittle:"Welcome to the blog"},
+    {id:2,tittle:"my second post"}
+]
+app.get('/api/v1/post',(req,res)=>{
+    res.json(post)
+})
+app.post('/api/v1/posts',(req,res)=>{
+    res.send('The post is made successfully.')
+})
 app.listen(PORT, ()=>{
     console.log("Server is runnig at http://localhost:"+PORT)
 })

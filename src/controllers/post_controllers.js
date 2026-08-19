6203f1fb23f0ad3e938acd8ef29f0a40b6bb4434
Jatin Yadav <jatinrao@7458>
2026-08-19 20:30:08 +0530

    feat/ added the query sorting

--- src/controllers/post_controllers.js
+++ src/controllers/post_controllers.js
@@ -1,5 +1,18 @@
+let posts=[
+    {id:1, tittle:"My first post",date:'2026-08-19'},
+    {id:2, tittle:"My second post",date:'2026-08-20'},
+]
 const getAllPosts= (req,res)=>{
-    res.status(200).json({message:'All post feteched succesfully '})
+    const {sortby }=req.query
+if (sortby=='date'){
+    posts.sort((a,b)=>{
+        return new Date(b.date)-new Date(a.date)
+        console.log("posts have been sorted by date")
+    })
+}
+    res.status(200).json({message:'All post feteched succesfully ',
+        data: posts
+    })
 }
 
 module.exports={ getAllPosts}
